941952ef4c0878141742c96cbe7ccb2327bec487
Kamesh <47671765+kamzkrish@users.noreply.github.com>
2022-06-13 16:36:27 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//Time complexity
+
+var twoSum = function (nums, target) {
+  for (var i = 0; i < nums.length; i++) {
+    for (var j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        console.log([i, j]);
+      }
+    }
+  }
+};
+twoSum([2, 7, 11, 15], 9);
